244ffdaf73532aa7703f30dfae64197f2a7d11fe
AlyHammoud <99489154+AlyHammoud@users.noreply.github.com>
2022-05-25 00:31:19 +0300

    separate componets

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,17 @@
 <template>
+<AppHeader />
+
   <div class="w-full">
-    <div class="flex">
-      <div class="mx-auto">
-        <p class="text-2xl text-red-500 mb-10">{{ fname }} {{ lname }}</p>
-        <ul>
-          <li class="mb-5 flex justify-between" v-for="hero in dcHeros" :key="hero.name">
-            <p>{{ hero.name }}</p>
-            <button @click="removeHero(hero.name)">x</button>
-          </li>
-        </ul>
-        <form @submit.prevent="addHero" action="">
-          <input class="border mr-3 rounded" placeholder="Add new hero" type="text" name="" v-model="dcHero">
-          <button class="bg-gradient-to-r bg-red-600 text-white" type="submit">Add</button>
-        </form>
-      </div>
-    </div>
+    <DcHeros />
   </div>
 </template>
 
 
 <script>
+import AppHeader from "./components/AppHeader";
+import DcHeros from "./components/DcHeros";
+
 export default{
-  data(){
-    return{
-      dcHero: "",
-      fname: "My",
-      lname: "Nameu",
-      dcHeros:[
-        {name:"one"},
-        {name:"Two"},
-        {name:"Thre"},
-        {name:"Four"},
-      ]
-    }
-  },
-
-  methods:{
-    addHero: function(){
-      if(this.dcHero != ""){
-        this.dcHeros.unshift({name : this.dcHero});
-        this.dcHero = "";
-      }
-    },
-
-    removeHero: function(heroName){
-      this.dcHeros = this.dcHeros.filter(function(hero){
-        return heroName != hero.name
-      });
-    },
-  }
+  components: {AppHeader,DcHeros}
 }
 </script>
